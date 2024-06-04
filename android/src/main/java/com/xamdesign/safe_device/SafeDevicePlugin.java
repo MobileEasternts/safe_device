2b9ac0f06f261d36104b3a0f4885caa8abbd7100
MobileEasternts <161289258+MobileEasternts@users.noreply.github.com>
2024-06-04 16:16:48 +0530

    Update SafeDevicePlugin.java

--- android/src/main/java/com/xamdesign/safe_device/SafeDevicePlugin.java
+++ android/src/main/java/com/xamdesign/safe_device/SafeDevicePlugin.java
@@ -2,7 +2,6 @@ package com.xamdesign.safe_device;
 
 import android.content.Context;
 import android.content.DialogInterface;
-import android.location.Location;
 import android.view.View;
 
 import androidx.annotation.NonNull;
@@ -10,7 +9,6 @@ import androidx.annotation.NonNull;
 import com.xamdesign.safe_device.DevelopmentMode.DevelopmentModeCheck;
 import com.xamdesign.safe_device.Emulator.EmulatorCheck;
 import com.xamdesign.safe_device.ExternalStorage.ExternalStorageCheck;
-import com.xamdesign.safe_device.MockLocation.LocationAssistant;
 import com.xamdesign.safe_device.Rooted.RootedCheck;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
@@ -25,13 +23,12 @@ import io.flutter.plugin.common.MethodChannel.Result;
  */
 public class SafeDevicePlugin implements FlutterPlugin, MethodCallHandler {
     private Context context;
-    private static LocationAssistantListener locationAssistantListener;
+
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
 
         this.context = binding.getApplicationContext();
-        locationAssistantListener = new LocationAssistantListener(context);
         onStart();
         final MethodChannel channel = new MethodChannel(
                 binding.getBinaryMessenger(),
@@ -40,18 +37,7 @@ public class SafeDevicePlugin implements FlutterPlugin, MethodCallHandler {
         channel.setMethodCallHandler(this);
     }
 
-    // onstop
-    public static void onStop() {
-        if (locationAssistantListener != null) {
-            locationAssistantListener.getAssistant().stop();
-        }
-    }
-    // onstart
-    public static void onStart() {
-        if (locationAssistantListener != null) {
-            locationAssistantListener.getAssistant().start();
-        }
-    }
+ 
 
 
 
@@ -82,75 +68,4 @@ public class SafeDevicePlugin implements FlutterPlugin, MethodCallHandler {
 
 }
 
-class LocationAssistantListener implements LocationAssistant.Listener {
-    private final LocationAssistant assistant;
-    private boolean isMockLocationsDetected = false;
-    private String latitude;
-    private String longitude;
 
-    public LocationAssistantListener(Context context) {
-        assistant = new LocationAssistant(context, this, LocationAssistant.Accuracy.HIGH, 5000, false);
-        assistant.setVerbose(true);
-        assistant.start();
-    }
-
-    @Override
-    public void onNeedLocationPermission() {
-        assistant.requestLocationPermission();
-        assistant.requestAndPossiblyExplainLocationPermission();
-    }
-
-    @Override
-    public void onExplainLocationPermission() {
-        io.flutter.Log.i("i", "onExplainLocationPermission: ");
-    }
-
-    @Override
-    public void onLocationPermissionPermanentlyDeclined(View.OnClickListener fromView, DialogInterface.OnClickListener fromDialog) {
-        io.flutter.Log.i("i", "onLocationPermissionPermanentlyDeclined: ");
-    }
-
-    @Override
-    public void onNeedLocationSettingsChange() {
-        io.flutter.Log.i("i", "LocationSettingsStatusCodes.RESOLUTION_REQUIRED: Please Turn on GPS location service.");
-    }
-
-    @Override
-    public void onFallBackToSystemSettings(View.OnClickListener fromView, DialogInterface.OnClickListener fromDialog) {
-        io.flutter.Log.i("i", "onFallBackToSystemSettings: ");
-    }
-
-    @Override
-    public void onNewLocationAvailable(Location location) {
-        if (location == null) return;
-        latitude = location.getLatitude() + "";
-        longitude = location.getLongitude() + "";
-        isMockLocationsDetected = false;
-    }
-
-    @Override
-    public void onMockLocationsDetected(View.OnClickListener fromView, DialogInterface.OnClickListener fromDialog) {
-        isMockLocationsDetected = true;
-    }
-
-    @Override
-    public void onError(LocationAssistant.ErrorType type, String message) {
-        io.flutter.Log.i("i", "Error: " + message);
-    }
-
-    public String getLatitude() {
-        return latitude;
-    }
-
-    public String getLongitude() {
-        return longitude;
-    }
-
-    public boolean isMockLocationsDetected() {
-        return isMockLocationsDetected;
-    }
-
-    public LocationAssistant getAssistant() {
-        return assistant;
-    }
-}
